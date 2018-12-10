17ecd0d87696b52a5af3c188f808756b8660f264
kuzsergey <kuzsergeyv@yandex.ru>
2018-12-10 17:15:49 +0400

    Changed Order Migration

--- database/migrations/2018_09_28_202618_create_orders_table.php
+++ database/migrations/2018_09_28_202618_create_orders_table.php
@@ -17,9 +17,18 @@ class CreateOrdersTable extends Migration
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('address_id');
+            $table->integer('status_id');
+            $table->integer('carrier_id');
             $table->string('comment');
             $table->text('shipping_address');
             $table->decimal('total', 8, 2);
+            $table->decimal('total_tax', 8, 2);
+            $table->decimal('total_shipping', 8, 2);
+            $table->decimal('total_shipping_tax', 8, 2);
+            $table->decimal('total_discount', 8, 2);
+            $table->decimal('total_discount_tax', 8, 2);
+            $table->string('shipping_no')->nullable();
+            $table->string('invoice_no')->nullable();
             $table->timestamps();
         });
     }
