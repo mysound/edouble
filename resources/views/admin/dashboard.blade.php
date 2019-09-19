2e456172fcc0c430ded3860a0bf419fb171db864
kuzsergey <kuzsergeyv@yandex.ru>
2019-09-19 22:44:44 +0400

    Changed Export

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -26,47 +26,70 @@
 		</div>
 		<div class="row">
 			<div class="col-sm-6">
-				<span class="btn btn-block btn-default">Downloads</span>
-				<span class="list-group-item">
-					<h4 class="list-group-item-heading">All Items</h4>
+				<span class="btn btn-block btn-default">Vendors</span>
+				{{-- <span class="list-group-item">
+					<h4 class="list-group-item-heading">All Items 2019 Year</h4>
 					<p class="list-group-item-text">
 						Amount Items ({{ $countpro }})
 						<span>@if($allProducts)<a href="{{ route('admin.download', ['fileTitle' => 'Products']) }}">{{ $allProducts }} <i class="fa fa-download"></i></a>@endif</span>
-						<a href="{{ route('admin.export', ['skuTitle' => '']) }}" class="pull-right"><i class="fa fa-refresh"></i></a>
+						@if(!$jobs)<a href="{{ route('admin.export', ['skuTitle' => '']) }}" class="pull-right"><i class="fa fa-refresh"></i></a>@endif
 					</p>
 				</span>
 				<span class="list-group-item">
-					<h4 class="list-group-item-heading">Redeye</h4>
+					<h4 class="list-group-item-heading">Redeye 2019 Year</h4>
 					<p class="list-group-item-text">
 						Amount Items
+						<span>@if($redeye)<a href="{{ route('admin.download', ['fileTitle' => 'REDEYE']) }}">{{ $redeye }} <i class="fa fa-download"></i></a>@endif</span>
+						@if(!$jobs)<a href="{{ route('admin.export', ['skuTitle' => 'REDEYE']) }}" class="pull-right"><i class="fa fa-refresh"></i></a>@endif
 					</p>
 				</span>
 				<span class="list-group-item">
-					<h4 class="list-group-item-heading">Secretly</h4>
+					<h4 class="list-group-item-heading">Secretly 2019 Year</h4>
 					<p class="list-group-item-text">
 						Amount Items
+						<span>@if($secretly)<a href="{{ route('admin.download', ['fileTitle' => 'SECRETLY']) }}">{{ $secretly }} <i class="fa fa-download"></i></a>@endif</span>
+						@if(!$jobs)<a href="{{ route('admin.export', ['skuTitle' => 'SECRETLY']) }}" class="pull-right"><i class="fa fa-refresh"></i></a>@endif
 					</p>
+				</span> --}}
+				<span class="list-group-item">
+					<h4 class="list-group-item-heading">Nullify Quantity</h4>
+					<form class="form-inline" method="POST" action="{{ route('admin.nullify') }}">
+						{{ csrf_field() }}
+						<div class="form-group">
+							<select class="form-control" name="catTitle">
+								<option disabled>-- Select Catalog --</option>
+								<option value="SECRETLY">SECRETLY</option>
+								<option value="REDEYE">REDEYE</option>
+							</select>
+						</div>
+						<button class="btn btn-primary" type="submit">Change Quantity</button>
+					</form>
 				</span>
 			</div>
 			<div class="col-sm-6">
-				<span class="btn btn-block btn-default">Sales</span>
+				<span class="btn btn-block btn-default">Reports</span>
 				<span class="list-group-item">
-					<h4 class="list-group-item-heading">All Items</h4>
-					<p class="list-group-item-text">
-						Amount Items
-					</p>
-				</span>
-				<span class="list-group-item">
-					<h4 class="list-group-item-heading">Redeye</h4>
-					<p class="list-group-item-text">
-						Amount Items
-					</p>
-				</span>
-				<span class="list-group-item">
-					<h4 class="list-group-item-heading">Secretly</h4>
-					<p class="list-group-item-text">
-						Amount Items
-					</p>
+					<form method="GET" action="{{ route('admin.export') }}">
+						{{ csrf_field() }}
+						<div class="form-group">
+							<label class="sr-only" for="year">Year</label>
+							<input type="text" class="form-control" name="year" id="year" placeholder="Year" required="">
+						</div>
+						<div class="form-group">
+							<label class="sr-only" for="month">Month</label>
+							<input type="text" class="form-control" name="month" id="month" placeholder="Month" required="">
+						</div>
+						<div class="form-group">
+							<select class="form-control" name="skuTitle">
+								<option value="">-- All Vendors --</option>
+								<option value="REDEYE">REDEYE</option>
+								<option value="SECRETLY">SECRETLY</option>
+							</select>
+						</div>
+						<hr>
+						<input class="btn btn-primary" type="submit" value="Download For eBay">
+						<hr>
+					</form>
 				</span>
 			</div>
 		</div>
